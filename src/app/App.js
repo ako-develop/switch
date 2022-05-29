0709354613241ddc9e28164cd51301e893951e57
Anton K <antonk@MacBook-Air-Anton.local>
2022-05-29 21:21:10 +0300

    link

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,10 @@
 import NavBar from "./components/navBar";
 import {Route, Switch} from "react-router-dom";
-import Dashboard from "./components/dashboard"
-import Login from "./components/login"
-import Posts from "./components/posts"
-import Home from "./components/home"
-import Stats from "./components/stats"
+import Dashboard from "./components/dashboard";
+import Login from "./components/login";
+import Posts from "./components/posts";
+import Home from "./components/home";
+import Stats from "./components/stats";
 
 
 
@@ -13,13 +13,15 @@ function App() {
     <div>
       <NavBar />
       <h1>App</h1>
-      <Switch>
-        <Route path="/" exact component={Home}/>
-        <Route path="/dashboard/stats" component={Stats}/>
-        <Route path="/dashboard" component={Dashboard}/>
-        <Route path="/login" component={Login}/>
-        <Route path="/posts" component={Posts}/>  
-      </Switch>
+      
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/dashboard/stats" component={Stats}/>
+          <Route path="/dashboard" component={Dashboard}/>
+          <Route path="/login" component={Login}/>
+          <Route path="/posts" component={Posts}/>  
+         
+        </Switch>
     </div>  
   );
 }
