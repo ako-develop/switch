52c3a9da2bbd76f80506268b646c1290bd51f3f9
Anton K <antonk@MacBook-Air-Anton.local>
2022-06-22 00:01:18 +0300

    err shoot

--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -1,13 +1,17 @@
 import PostsList from "./postsList";
 import Post from "./post";
+import query from "query-string";
 
-const Posts = ({match}) => {
+
+const Posts = ({match, location}) => {
     const posts=[
         {id:1, label:"post 1"},
         {id:2, label:"post 2"},
         {id:3, label:"post 3"},
       ];
-      const postId=match.params.postId
+      const search = location.search;
+      const postId=match.params.postId;
+      console.log(search);
       return <> {postId ? <Post posts={posts} id={postId}/> : <PostsList posts={posts}/>}</>;
 };
  
